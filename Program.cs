e01493df195c7a10663cae58b336735ec14938bc
jorgef478 <jorgef478@gmail.com>
2023-11-17 20:09:49 -0300

    Finalizado!

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int numero = -1;
+
+Console.WriteLine("~~~~ Entrada Valida ~~~~ ");
+
+while (numero < 0 || numero > 9)
+{
+Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+numero = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numero == 0)
+{
+    Console.WriteLine("Operação cancelada.");
+    Console.WriteLine("Volte Sempre! =)");
+}
+else
+{
+    Console.WriteLine($"O número selecionado é: {numero}");
+    Console.WriteLine("Volte Sempre! =)");
+}
